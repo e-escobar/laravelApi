0ff7e8beec1f6895b63bc7da1b289204df4bcd94
Eric Escobar Mendoza <eescobar@cdhidalgo.tecnm.mx>
2024-10-17 09:56:48 -0600

    Autenticación con Sanctum, Función Store de Controlador de Login

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoriaController;
 use App\Http\Controllers\Api\RecetaController;
 use App\Http\Controllers\Api\EtiquetaController;
+use App\Http\Controllers\Api\LoginController;
 
-Route::get('categorias', [CategoriaController::class, 'index']);
-Route::get('categorias/{categoria}', [CategoriaController::class, 'show']);
+Route::post('login', [LoginController::class, 'store']);
 
-Route::apiResource('recetas', RecetaController::class);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('categorias', [CategoriaController::class, 'index']);
+    Route::get('categorias/{categoria}', [CategoriaController::class, 'show']);
+    
+    Route::apiResource('recetas', RecetaController::class);
+    
+    Route::get('etiquetas', [EtiquetaController::class, 'index']);
+    Route::get('etiquetas/{etiqueta}', [EtiquetaController::class, 'show']);
 
-Route::get('etiquetas', [EtiquetaController::class, 'index']);
-Route::get('etiquetas/{etiqueta}', [EtiquetaController::class, 'show']);
+});
+
 
 /*
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
