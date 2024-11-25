488002ac3d76a147da8628a21ceaa6ae8a106465
Eric Escobar Mendoza <eescobar@cdhidalgo.tecnm.mx>
2024-11-24 22:30:19 -0600

    Corrección AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,10 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
-        
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
